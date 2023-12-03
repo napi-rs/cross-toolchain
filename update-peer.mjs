af0c6dbc0294e9f8bdc9975f056be25e2ba54be5
LongYinan <lynweklm@gmail.com>
2023-12-03 16:42:33 +0800

    Update peer before publish

---
+++ update-peer.mjs
@@ -0,0 +1,14 @@
+import { writeFileSync } from 'node:fs'
+import { join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+import rootPkgJson from './package.json' assert { type: 'json' }
+
+const __dirname = join(fileURLToPath(import.meta.url), '..')
+
+rootPkgJson.peerDependencies = Object.keys(rootPkgJson.peerDependencies).reduce((acc, name) => {
+  acc[name] = `^${rootPkgJson.version}`
+  return acc
+}, {})
+
+writeFileSync(join(__dirname, 'package.json'), JSON.stringify(rootPkgJson, null, 2) + '\n', 'utf8')
