4c0a52a7a7fe41f59da5918014bfdf1b77188bf7
LongYinan <lynweklm@gmail.com>
2023-12-04 22:57:45 +0800

    Remove platform restriction in @napi-rs/cross-toolchain

--- index.d.ts
+++ index.d.ts
@@ -4,3 +4,11 @@ export const arm64TargetArmv7: string | undefined
 export const x64TargetX86_64: string | undefined
 export const x64TargetAarch64: string | undefined
 export const x64TargetArmv7: string | undefined
+export type PlatformToolchain = {
+  [key in
+    | 'armv7-unknown-linux-gnueabihf'
+    | 'aarch64-unknown-linux-gnu'
+    | 'x86_64-unknown-linux-gnu']: string | undefined
+}
+export const arm64: PlatformToolchain
+export const x64: PlatformToolchain
--- index.js
+++ index.js
@@ -48,3 +48,14 @@ module.exports.arm64TargetArmv7 = arm64TargetArmv7
 module.exports.x64TargetX86_64 = x64TargetX86_64
 module.exports.x64TargetAarch64 = x64TargetAarch64
 module.exports.x64TargetArmv7 = x64TargetArmv7
+module.exports.arm64 = {
+  'armv7-unknown-linux-gnueabihf': arm64TargetArmv7,
+  'aarch64-unknown-linux-gnu': arm64TargetAarch64,
+  'x86_64-unknown-linux-gnu': arm64TargetX86_64
+}
+
+module.exports.x64 = {
+  'armv7-unknown-linux-gnueabihf': x64TargetArmv7,
+  'aarch64-unknown-linux-gnu': x64TargetAarch64,
+  'x86_64-unknown-linux-gnu': x64TargetX86_64
+}
