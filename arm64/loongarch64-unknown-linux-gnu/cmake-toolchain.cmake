6fcca978b8974fb2ab68331fd1840444d94430be
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2025-09-18 11:20:50 +0800

    chore: update toolchain

---
+++ arm64/loongarch64-unknown-linux-gnu/cmake-toolchain.cmake
@@ -0,0 +1,5 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR loongarch64)
+set(CMAKE_SYSROOT /usr/loongarch64-unknown-linux-gnu/loongarch64-unknown-linux-gnu/sysroot/)
+set(CMAKE_C_COMPILER loongarch64-unknown-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER loongarch64-unknown-linux-gnu-g++)
---
+++ arm64/loongarch64-unknown-linux-gnu/index.js
@@ -0,0 +1,1 @@
+module.exports.toolchainPath = require('node:path').join(__dirname, 'loongarch64-unknown-linux-gnu.tar.xz')
